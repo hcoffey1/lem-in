bbb446db6bad371351fd7d0873f4ae346bea6f28
smorty <hauh@ya.ru>
2019-07-19 00:04:48 +0300

    graph store

--- include/lemin.h
+++ include/lemin.h
@@ -6,20 +6,25 @@
 /*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 23:36:34 by smorty            #+#    #+#             */
-/*   Updated: 2019/07/18 00:12:55 by smorty           ###   ########.fr       */
+/*   Updated: 2019/07/18 23:18:35 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 
+#include <stdio.h> //remove
+
 typedef struct	s_room
 {
 	char			*name;
 	int				x;
 	int				y;
-	int				status;
-	struct s_lemin	**next;
+	int				type;
+	int				edges;
+	struct	s_room	*right;
+	struct	s_room	*left;
+	void			**links;
 }				t_room;
 
 typedef struct	s_lemin
--- srcs/read_file.c
+++ srcs/read_file.c
@@ -6,7 +6,7 @@
 /*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 23:50:49 by smorty            #+#    #+#             */
-/*   Updated: 2019/07/18 00:19:52 by smorty           ###   ########.fr       */
+/*   Updated: 2019/07/19 00:03:42 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 #include "get_next_line.h"
 
 /*
-** status: 0 - none, 1 - start, 2 - end, 3 - comment
+** type: -1 - unknown, 0 - connection, 1 - comment, 2 - vertex, 3 - start, 4 - end
 */
 
-static t_room	*read_line(char *line, int status)
+static t_room	*read_line(t_room *rooms, char *line, int type)
 {
 	t_room	*new;
-	int		spaces;
 	char	**split;
 
-	spaces = 0;
-	new = (t_room *)malloc(sizeof(t_room));
-	new->status = status;
-	if (!(split = ft_strsplit(line, ' ')))
+	if (!(new = (t_room *)malloc(sizeof(t_room))))
 		exit(-1);
-	while (*line)
-		if (*line++ == ' ')
-			++spaces;
-	if (spaces != 3)
+	new->links = NULL;
+	new->right = NULL;
+	if ((new->left = rooms))
+		rooms->right = new;
+	new->type = type;
+	new->edges = 0;
+	if (!(split = ft_strsplit(line, ' ')))
 		exit(-1);
 	new->name = ft_strdup(*split);
 	free(*split);
@@ -40,47 +39,105 @@ static t_room	*read_line(char *line, int status)
 	new->y = ft_atoi(*(split + 2));
 	free(*(split + 2));
 	free(split);
+	return (new);
 }
 
-static int		get_status(char *line)
+static int		check_line(char *line)
 {
-	int status;
+	int type;
 
-	status = 0;
+	type = 0;
 	if (*line == '#')
 	{
-		if (*(line + 1) != '#')
-			status = 3;
-		else if (ft_strequ(line, "start"))
-			status = 1;
-		else if (ft_strequ(line, "start"))
-			status = 2;
+		if (ft_strequ(line, "##start"))
+			return (3);
+		else if (ft_strequ(line, "##end"))
+			return (4);
 		else
-			exit(-1);
+			return(1);
 	}
-	return (status);
+	else
+		while (*line)
+			if (*line++ == ' ')
+				++type;
+	if (type && type != 2)
+		exit(-1);
+	return (type);
 }
 
-void			read_file(int fd)
+static void		connect(t_room *rooms1, char *line, int count)
+{
+	t_room	*rooms2;
+	char	**split;
+
+	rooms2 = rooms1;
+	if (!(split = ft_strsplit(line, '-')))
+		exit(-1);
+	if (!*split || !*(split + 1) || *(split + 2))
+		exit(-1);
+	while (rooms1 && !ft_strequ(rooms1->name, *split))
+		rooms1 = rooms1->left;
+	if (!rooms1)
+		exit(-1);
+	while (rooms2 && !ft_strequ(rooms2->name, *(split + 1)))
+		rooms2 = rooms2->left;
+	if (!rooms2)
+		exit(-1);
+	if (!rooms1->links)
+		rooms1->links = malloc(sizeof(t_room *) * (count + 1));
+	rooms1->links[rooms1->edges++] = (t_room *)rooms2;
+	rooms1->links[rooms1->edges] = NULL;
+	free(*split);
+	free(*(split + 1));
+	free(split);
+}
+
+t_room			*read_file(int fd, int *ants)
 {
 	char	*line;
-	int		ants;
 	int		r;
-	int		status;
-	t_room	*room;
+	int		type;
+	t_room	*rooms;
+	int		count;
 
-	r = get_next_line(fd, &line); //первая строка - количество муравьёв
-	ants = ft_atoi(line);
+	rooms = NULL;
+	r = get_next_line(fd, &line);
+	*ants = ft_atoi(line);
 	free(line);
-	while (r = get_next_line(fd, &line))
+	count = 0;
+	type = -1;
+	while ((r = get_next_line(fd, &line)))
 	{
-		status = 0;
 		if (r < 0)
 			exit(-1);
-		if (*line == '#')
-			status = get_status(line);
-		else
-			room = read_line(line, status);
+		if (type)
+		{
+			type = check_line(line);
+			if (type == 2)
+			{
+				rooms = read_line(rooms, line, type);
+				++count;
+				type = -1;
+			}
+		}
+		else if (*line != '#')
+			connect(rooms, line, count);
 		free(line);
 	}
+	while (rooms->left)
+		rooms = rooms->left;
+	return (rooms);
+}
+
+int main(int argc, char **argv)
+{
+	t_room *rooms;
+	int		fd;
+	int		ants;
+
+	if (argc != 2)
+		return (0);
+	fd = open(*(argv + 1), O_RDONLY);
+	rooms = read_file(fd, &ants);
+	close(fd);
 }
