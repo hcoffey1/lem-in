70d62ce7df3d579b290479537a801f86e9fc703e
smorty <hauh@ya.ru>
2019-07-27 15:53:30 +0300

    new header

--- include/lemin.h
+++ include/lemin.h
@@ -5,99 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/17 23:36:34 by smorty            #+#    #+#             */
-/*   Updated: 2019/07/26 22:46:27 by smorty           ###   ########.fr       */
+/*   Created: 2019/07/27 15:35:02 by smorty            #+#    #+#             */
+/*   Updated: 2019/07/27 15:53:17 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <mlx.h>
-#include <math.h>
-
-#define X 0
-#define Y 1
-#define LEMIN_VIS_WIDTH 1920
-#define LEMIN_VIS_HEIGHT 1200
-
-typedef struct	s_lemin
-{
-	void			*mlx_p;
-	void			*win_p;
-    void			*img_p;
-	char			*map;
-	int				bpp;
-	int				size_line;
-	int				endian;
-	int				width;
-	int				height;
-}				t_lemin;
-
-typedef struct	s_edge
-{
-	int				dir;
-	int				weight;
-	struct s_edge	*next;
-	struct s_vertex *right;
-	struct s_vertex	*left;
-}				t_edge;
+# define INF_WEIGHT	0xABCDEF
 
 typedef struct	s_vertex
 {
 	char			*name;
+	int				index;
+	int				type;
 	int				x;
 	int				y;
-	int				type;
 	int				visited;
-	struct s_links	*edges;
+	struct s_vertex	*next;
+	struct s_vertex	*prev;
 }				t_vertex;
 
-typedef struct	s_links
+typedef struct	s_edge
 {
-	t_edge			*edge;
-	struct s_links	*next;
-}				t_links;
+	int				index;
+	int				weight;
+	int				direction;
+	struct s_edge	*next;
+	struct s_edge	*prev;
+}				t_edge;
 
 typedef struct	s_queue
 {
-	t_vertex		*val;
-	struct s_queue	*right;
-	struct squeue	*left;
+	t_vertex		*top;
+	struct s_queue	*next;
+	struct s_queue	*prev;
 }				t_queue;
 
-typedef struct	s_path_list
+typedef struct	s_paths
 {
-	t_queue				*path;
-	int					len;
-	struct s_path_list	*right;
-	struct s_path_list	*left;
-}				t_path_list;
+	t_queue			*path;
+	int				len;
+	struct s_paths	*next;
+	struct s_paths	*prev;
+}				t_paths;
 
 typedef struct	s_ants
 {
-	int					num;
-	t_vertex			*room;
-	t_queue				*path;
-	struct s_ants		*next;
+	int				num;
+	t_vertex		*room;
+	t_queue			*path;
+	struct s_ants	*next;
 }				t_ants;
 
-void			validate(int fd, int *ants, int *verteces, int *connects);
-t_vertex		**read_file(int fd, int verteces, int connects);
-t_queue			*bfs(t_vertex *start);
-t_queue			*new_queue(t_vertex *start);
-void			push(t_queue **q, t_vertex *val);
-void			push_front(t_queue **q, t_vertex *val);
-void			pop(t_queue **q);
-t_path_list		*pathfinding(t_vertex **list_nodes);
-void			open_the_gates(t_path_list *path_list, int ants);
-int				ft_printf(const char *s, ...);
-void			error(int error_type);
-void			ft_bzero(void *s, size_t n);
-void			visualize_graph(t_vertex **list_vertexs);
-void			dijkstra(t_vertex *start);
-void			bellman_ford(t_vertex **list_vertexs);
+typedef struct	s_lemin
+{
+	t_vertex		*rooms;
+	t_edge			**adjacency;
+	t_vertex		*start;
+	t_vertex		*end;
+	int				ants_num;
+	t_ants			*ants;
+	t_paths			*path_list;
+}				t_lemin;
 
 #endif
---
+++ srcs/old/lemin.h
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lemin.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 23:36:34 by smorty            #+#    #+#             */
+/*   Updated: 2019/07/27 15:34:03 by smorty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEMIN_H
+# define LEMIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <mlx.h>
+#include <math.h>
+
+#define X 0
+#define Y 1
+#define LEMIN_VIS_WIDTH 1920
+#define LEMIN_VIS_HEIGHT 1200
+
+typedef struct	s_vis
+{
+	void			*mlx_p;
+	void			*win_p;
+    void			*img_p;
+	char			*map;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	int				width;
+	int				height;
+}				t_vis;
+
+typedef struct	s_edge
+{
+	int				dir;
+	int				weight;
+	struct s_edge	*next;
+	struct s_vertex *right;
+	struct s_vertex	*left;
+}				t_edge;
+
+typedef struct	s_vertex
+{
+	char			*name;
+	int				x;
+	int				y;
+	int				type;
+	int				visited;
+	struct s_links	*edges;
+}				t_vertex;
+
+typedef struct	s_links
+{
+	t_edge			*edge;
+	struct s_links	*next;
+}				t_links;
+
+typedef struct	s_queue
+{
+	t_vertex		*val;
+	struct s_queue	*right;
+	struct squeue	*left;
+}				t_queue;
+
+typedef struct	s_path_list
+{
+	t_queue				*path;
+	int					len;
+	struct s_path_list	*right;
+	struct s_path_list	*left;
+}				t_path_list;*/
+
+typedef struct	s_ants
+{
+	int					num;
+	t_vertex			*room;
+	t_queue				*path;
+	struct s_ants		*next;
+}				t_ants;
+
+void			validate(int fd, int *ants, int *verteces, int *connects);
+t_vertex		**read_file(int fd, int verteces, int connects);
+t_queue			*bfs(t_vertex *start);
+t_queue			*new_queue(t_vertex *start);
+void			push(t_queue **q, t_vertex *val);
+void			push_front(t_queue **q, t_vertex *val);
+void			pop(t_queue **q);
+t_path_list		*pathfinding(t_vertex **list_nodes);
+void			open_the_gates(t_path_list *path_list, int ants);
+int				ft_printf(const char *s, ...);
+void			error(int error_type);
+void			ft_bzero(void *s, size_t n);
+void			visualize_graph(t_vertex **list_vertexs);
+void			dijkstra(t_vertex *start);
+void			bellman_ford(t_vertex **list_vertexs);
+
+#endif
